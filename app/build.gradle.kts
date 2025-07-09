877fb9f746adaab20fe776f0653696509ba847e8
shazrin <mshazrinbs@gmail.com>
2025-07-09 13:58:06 +0800

    build: remove group name in app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,8 +4,6 @@ plugins {
     id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "io.github.shazxrin"
-
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(24)
